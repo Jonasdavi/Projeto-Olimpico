602e2768c82ff095c221865418b9dba1de7ab56a
Jonasdavi <jonas.davi@escolar.ifrn.edu.br>
2025-09-06 05:23:12 -0300

    update

--- projeto-genilton/backend/database/tabelas.js
+++ projeto-genilton/backend/database/tabelas.js
@@ -1,11 +1,15 @@
 import { sql } from "./conexao.js";
 
-export class aluno{
+export class TabelaAluno{
     async add(usuario){
 
-        const {id, nome, email, senha} = usuario;
+        const {nome, professores, data_nascimento, genero, cpf, matricula, campus, curso, turno, ano_escolar, tipo_escola, email, whatsapp, senha, medalhas, olimpiadas} = usuario;
         await sql`
-            INSERT INTO public."cadastro-basico" (id, nome, email, senha) VALUES (${id}, ${nome}, ${email}, ${senha})
+
+            INSERT INTO alunos (nome, professores, data_nascimento, genero, cpf, matricula, campus, curso, turno, ano_escolar, tipo_escola, email, whatsapp, senha, medalhas, olimpiadas) VALUES (${nome}, ARRAY[${professores.map(item=>{
+                
+            })}], ${data_nascimento}, ${genero}, ${cpf}, ${matricula}, ${campus}, ${curso}, ${turno}, ${ano_escolar}, ${tipo_escola}, ${email}, ${whatsapp}, ${senha}, ARRAY[${medalhas}], ARRAY[${olimpiadas}])
+
         `; 
     }
 
@@ -36,7 +40,7 @@ export class aluno{
         //usuarios = this.#filtrarUsuarios(usuarios, filtro, comSenha); 
         
         return usuarios;
-        
+
     }
 
     #filtrarUsuarios(array, search, comSenha) { //filtra em função do id ou nome ou email e nao envia a senha junto
@@ -49,6 +53,6 @@ export class aluno{
     }
 }
 
-export class professor{
+export class TabelaProfessor{
 
 }
--- projeto-genilton/backend/server.js
+++ projeto-genilton/backend/server.js
@@ -1,3 +1,66 @@
 import {fastify} from 'fastify';
 import fastifyCors from '@fastify/cors';
-import {DatabaseCadastro} from './banco-de-dados/bd.js'
+import {TabelaAluno, TabelaProfessor} from './database/tabelas.js'
+
+const server= fastify();
+
+const tAluno= new TabelaAluno();
+const tProf= new TabelaProfessor();
+
+server.register(fastifyCors, {
+    origin : '*' //permite qq outro endereço fazer a requisição
+});
+
+server.post('/aluno', async (res, rep)=>{
+    await tAluno.add(res.body);
+
+    
+
+    return rep.send(res.body)
+})
+server.post('/professor', async (res, rep)=>{
+
+
+    
+
+    return res.body
+})
+
+server.get('/', async (res, rep)=>{
+
+    return rep.send(res.body)
+})
+
+server.put('/', async ()=>{
+    return ''
+})
+
+server.patch('/', async ()=>{
+    return ''
+})
+
+server.delete('/', async ()=>{
+    return ''
+})
+
+server.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ?? 3333
+})
+const teste={
+    "nome" : "Jonas Davi de Souza Rocha",
+    "professores" : ["Genilton", "Macedo"],
+    "data_nascimento" : "2006-08-22",
+    "genero" : "Masculino",
+    "cpf" : "123456789",
+    "matricula" : "20231164010003",
+    "campus" : "SGA",
+    "curso" : "info",
+    "turno" : "Vespertino",
+    "ano_escolar" : "3 do médio",
+    "email" : "jonas.davi@escolar.ifrn",
+    "whatsapp" : "84999434390",
+    "senha" : "123456",
+    "medalhas" : ["teste1", 1],
+    "olimpiadas" : ["teste1", 1, "teste1"]
+}
